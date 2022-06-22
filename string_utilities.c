8cb1a11048b820859cbb59cb70228e75262af260
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2022-06-22 18:06:28 +0530

    Update string_utilities.c

--- string_utilities.c
+++ string_utilities.c
@@ -19,3 +19,33 @@ void print_till_x(char* str,int x) // prints the string only till the xth charac
         j++;
     }
 }
+int srsl(char* str) //return the size of smallest repeating string which forms thr string str (basically [str,str+p) is the smallest repeating string)
+{
+    int j=0;
+    while(str[j]!='\0')
+    j++;
+    int n=j;
+    int p=1;
+    j=1;
+    int flag=0;
+    while(str[j]!='\0' && flag==0)
+    {
+        
+        if(n%p==0)
+        {
+            int k=p;
+            while(k<n && check(str,p,str+k)==1)
+            k=k+p;
+            if(k==n)
+            {
+                flag=1;
+                break;
+            }
+            else
+            p++;
+        }
+        else
+        p++;
+    }
+    return p;
+}
