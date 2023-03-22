e22bb7d9d2668c9384fbbfa0aa6e2ed17a1feaf6
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-03-22 11:41:34 +0530

    Update math_and_longs.cpp

--- math_and_longs.cpp
+++ math_and_longs.cpp
@@ -8,3 +8,18 @@ int positive_modulo(long long int x,long long int y)
     long long z=((x%y)+y)%y;
     return z;
 }
+long long power(long long x,long long a,long long mod)
+{
+    if(a==0)
+    return 1;
+    else
+    {
+        long long p=power(x,a/2);
+        p=p*p;
+        p=p%mod;
+        if(a%2==1)
+        p=p*x;
+        p=p%mod;
+        return p;
+    }
+}
