7621671228f69599662574ee30cdd2efb0dfc603
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-08-04 00:33:45 +0530

    Update print_all_permutations.cpp

--- print_all_permutations.cpp
+++ print_all_permutations.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
 using namespace std;
 void print_array(int* arr,int n)
 {
@@ -80,12 +81,62 @@ void print_permutations(int* arr,int n)
 {
     permfunc(arr,n,0,0);
 }
+void uniquepermfunc(int* rem,int n,int* perm,int p)
+{
+    if(n==0)
+    {
+        print_array(perm,p);
+        printf("\n");
+    }
+    else
+    {
+        int j=0;
+        while(j<n)
+        {
+            int x=rem[j];
+            int* newrem;
+            if(n>1)
+            newrem=(int*)malloc(sizeof(int)*(n-1));
+            else
+            newrem=0;
+            int k=0;
+            int l=0;
+            while(k<n)
+            {
+                if(k!=j)
+                {
+                    newrem[l]=rem[k];
+                    k++;
+                    l++;
+                }
+                else
+                k++;
+            }
+            int* newperm=(int*)malloc(sizeof(int)*(p+1));
+            k=0;
+            while(k<p)
+            {
+                newperm[k]=perm[k];
+                k++;
+            }
+            newperm[k]=x;
+            uniquepermfunc(newrem,n-1,newperm,p+1);
+            while(j<n && rem[j]==x)
+            j++;
+        }
+    }
+}
+void print_unique_permutations(int* arr,int n)
+{
+    sort(arr,arr+n);
+    uniquepermfunc(arr,n,0,0);
+}
 int main() 
 {
     int n;
     scanf("%d ",&n);
     int arr[n];
     scan_array(arr,n);
-    print_permutations(arr,n);
+    print_unique_permutations(arr,n);
     return 0;
 }
