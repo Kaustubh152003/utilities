5cd1942f56bff64dc0f8c70bd7ccfad6b0e5368f
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-08-15 13:15:46 +0530

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -58,6 +58,64 @@ int** convert_vm_to_am(vector<vector<int>> mat,int* x,int* y)
     return ans;
     
 }
+int** give_identity(int x,int y)
+{
+    int** ans=(int**)malloc(x*sizeof(int*));
+    int j=0;
+    while(j<x)
+    {
+        int k=0;
+        int* ptr=(int*)malloc(sizeof(int)*y);
+        while(k<y)
+        {
+            if(k==j)
+            ptr[k]=1;
+            else
+            ptr[k]=0;
+            k++;
+        }
+        ans[j]=ptr;
+        j++;
+    }
+    return ans;
+    
+}
+int** matrix_power_n(int** mat,int x,int y,int n)
+{
+    if(n==0)
+    return give_identity(x,y);
+    else
+    {
+        int** p=matrix_power_n(mat,x,y,n/2);
+        if(n%2==0)
+        {
+            int tx,ty;
+            int** ans= multiply_matrix(p,x,y,p,x,y,&tx,&ty);
+            return ans;
+        }
+        else
+        {
+            int tx,ty;
+            int** ans= multiply_matrix(p,x,y,p,x,y,&tx,&ty);
+            ans=multiply_matrix(ans,x,y,mat,x,y,&tx,&ty);
+            return ans;
+        }
+    }
+    
+}
+int FindNthTerm(long long int n) 
+{
+    int reqsize=2;
+    int f0=1;
+    int f1=1;
+    int** mat=(int**)malloc(2*sizeof(int*));
+    int a1[2]={1,1};
+    int a2[2]={1,0};
+    mat[0]=a1;
+    mat[1]=a2;
+    int** coe=matrix_power_n(mat,2,2,n-1);
+    return coe[0][0]*f1+coe[0][1]*f0;
+}
 void print_matrix(int** mat,int n,int m)
 {
     int j=0;
