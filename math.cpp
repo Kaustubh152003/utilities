122ea5930f6aaa060a74e4dfd6d09b2954efd674
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-12-30 09:04:30 +0530

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -82,13 +82,15 @@ int nod(int x) //number of divisors
 {
     int j=1;
     int ans=0;
-    while(j<sqrt(x))
+    int limit=sqrt(x);
+    while(j<=sqrt(x))
     {
         if(x%j==0)
         ans=ans+2;
         j++;
     }
-    if(x%j==0 && j*j==x)
-    ans++;
+    j--;
+    if(j*j==x)
+    ans--;
     return ans;
 }
