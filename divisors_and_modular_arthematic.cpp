2af6359d18dfe8f6a2e1fbcf303a9c853d55ab83
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-12-30 09:07:54 +0530

    Update divisors_and_modular_arthematic.cpp

--- divisors_and_modular_arthematic.cpp
+++ divisors_and_modular_arthematic.cpp
@@ -1,9 +1,9 @@
-int nod(int x) //number of divisors
+int number_of_divisors(int x) //number of divisors
 {
     int j=1;
     int ans=0;
     int limit=sqrt(x);
-    while(j<=sqrt(x))
+    while(j<=limit)
     {
         if(x%j==0)
         ans=ans+2;
