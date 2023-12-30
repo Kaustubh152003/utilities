0e443fde06694118b2ff1bb01157c1b09860fb4b
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-12-30 09:21:28 +0530

    Update divisors_and_modular_arthematic.cpp

--- divisors_and_modular_arthematic.cpp
+++ divisors_and_modular_arthematic.cpp
@@ -14,3 +14,29 @@ int number_of_divisors(int x) //number of divisors
     ans--;
     return ans;
 }
+#define A 1000000007
+long long int sum_of_divisors(long long int x) //sum of divisors
+{
+    int j=1;
+    long long sum=0;
+    long long int limit=sqrt(x);
+    while(j<=limit)
+    {
+        if(x%j==0)
+        {
+            sum=sum+j;
+            sum=sum%A;
+            sum=sum+x/j;
+            sum=sum%A;
+        }
+        j++;
+    }
+    j--;
+    if(j*j==x)
+    {
+        sum=sum-j;
+        sum=sum%A;
+    }
+    sum=(sum+A)%A;
+    return sum;
+}
