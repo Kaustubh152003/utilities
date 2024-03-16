12d809c2b05e987262ab57eb1b8f8029f224ee5c
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2024-03-16 12:40:11 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,19 +1,26 @@
-struct Node* create(int x)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* create(int x)
 {
-    struct Node* ptr=(struct Node*)malloc(sizeof(struct Node));
+    struct ListNode* ptr=(struct ListNode*)malloc(sizeof(struct ListNode));
     ptr->next=0;
-    ptr->data=x;
+    ptr->val=x;
     return ptr;
 }
-struct Node *insertAtBegining(struct Node *head, int x) {
+struct ListNode *insertAtBegining(struct ListNode *head, int x) {
 
-    struct Node* ptr=create(x);
+    struct ListNode* ptr=create(x);
 	ptr->next=head;
 	return ptr;
 }
-struct Node *insertAtEnd(struct Node *head, int x)  {
-    struct Node* ptr=create(x);
-    struct Node* og=head;
+struct ListNode *insertAtEnd(struct ListNode *head, int x)  {
+    struct ListNode* ptr=create(x);
+    struct ListNode* og=head;
     if(head==0)
     return ptr;
     else
@@ -23,4 +30,28 @@ struct Node *insertAtEnd(struct Node *head, int x)  {
         head->next=ptr;
         return og;
     }
+}
+int size_of_ll(struct ListNode* head)
+{
+    int j=0;
+    while(head!=0)
+    {
+        head=head->next;
+        j++;
+    }
+    return j;
+}
+int* array_from_ll(struct ListNode* head,int* size_of_array)
+{
+    int j=0;
+    int n=size_of_ll(head);
+    int* arr=(int*)malloc(n*sizeof(int));
+    while(j<n)
+    {
+        arr[j]=head->val;
+        head=head->next;
+        j++;
+    }
+    *size_of_array=n;
+    return arr;
 }
