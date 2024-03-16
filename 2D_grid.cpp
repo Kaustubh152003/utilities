80e367c3a87f543849d77e223206a431ac5d8f34
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2024-03-16 12:58:05 +0530

    Create 2D_grid.cpp

---
+++ 2D_grid.cpp
@@ -0,0 +1,41 @@
+int** make_grid(int n,int m) //makes and returns a grid of size n x m
+{
+    int** table=(int**)malloc(sizeof(int*)*n);
+    int j=0;
+    while(j<n)
+    {
+        int* ptr=(int*)malloc(sizeof(int)*m);
+        table[j]=ptr;
+        j++;
+    }
+    return table;
+}
+void initialize_grid(int** table,int n,int m,int val)
+{
+    int j=0;
+    while(j<n)
+    {
+        int k=0;
+        while(k<m)
+        {
+            table[j][k]=val;
+            k++;
+        }
+        j++;
+    }
+}
+void print_grid(int** table,int n,int m)
+{
+    int j=0;
+    while(j<n)
+    {
+        int k=0;
+        while(k<m)
+        {
+            printf("%d ",table[j][k]);
+            k++;
+        }
+        printf("\n");
+        j++;
+    }
+}
